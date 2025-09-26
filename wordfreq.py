463d737ae4eabe5433084a68ebbf970f84b036bb
Johan <johan.svensson@gtsmarketing.se>
2025-09-26 11:55:50 +0200

    tokenize Done

--- wordfreq.py
+++ wordfreq.py
@@ -2,14 +2,24 @@ def tokenize(lines):
     words = []
     for line in lines:
         start = 0
-        while start < len(line): 
-            while line[start].isspace():
-                start = start +1
-            while line[start].isdigit():
-                if line[start]
+        while start < len(line):
+            while start < len(line) and line[start].isspace():
+                start += 1
+            if start >= len(line):
+                break
+            if line[start].isdigit():
+                end = start
+                while end < len(line) and line[end].isdigit():
+                    end += 1
+                words.append(line[start:end])
+                start = end
             elif line[start].isalpha():
-                print (f"{line[start]} is a letter")
+                end = start
+                while end < len(line) and line[end].isalpha():
+                    end += 1
+                words.append(line[start:end].lower())
+                start = end
             else:
-                print (f"{line[start]} is a letter")
-            start = start +1
+                words.append(line[start])
+                start += 1
     return words
