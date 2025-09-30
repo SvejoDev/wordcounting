530cb9611b2c3c7735d65333e540855719a42cd5
Victor Hieber <vg.hieber@gmail.com>
2025-09-30 16:13:24 +0200

    completed program

--- topmost.py
+++ topmost.py
@@ -6,20 +6,24 @@ def main():
 
     inp_file = open(sys.argv[2], encoding='utf-8')
 
+    list_inp_file = [] 
     for line in inp_file:
         striped_inp_lines = line.strip('\n')
+        list_inp_file.append(striped_inp_lines)
 
     inp_file.close()
 
     eng_stop = open(sys.argv[1], encoding = 'utf-8')
 
+    list_stop_file = []
     for line in eng_stop:
-        striped_stop_lines = line.strip('\n')
+        striped_stop_lines = line.strip()
+        list_stop_file.append(striped_stop_lines)
 
     eng_stop.close()
 
-    t = wordfreq.tokenize(striped_inp_lines)
-    c = wordfreq.countWords(t,striped_stop_lines)
+    t = wordfreq.tokenize(list_inp_file)
+    c = wordfreq.countWords(t,list_stop_file)
     wordfreq.printTopMost(c,int(sys.argv[3]))
 
 
