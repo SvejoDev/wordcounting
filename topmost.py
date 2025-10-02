81880b8e5c65689599cc716cf0225267de8de477
Johan <johan.svensson@gtsmarketing.se>
2025-10-02 11:03:14 +0200

    done

--- topmost.py
+++ topmost.py
@@ -2,16 +2,22 @@ import wordfreq
 import sys
 import urllib.request
 
+response = urllib.request.urlopen(sys.argv[2])
+lines = response.read().decode("utf8").splitlines()
+
 def main():
 
-    inp_file = open(sys.argv[2], encoding='utf-8')
+    if sys.argv[2].startswith("http"):
+        inp_file = lines
+    else:
+        inp_file = open(sys.argv[2], encoding='utf-8')
+        inp_file.close()
 
     list_inp_file = [] 
     for line in inp_file:
         striped_inp_lines = line.strip('\n')
         list_inp_file.append(striped_inp_lines)
 
-    inp_file.close()
 
     eng_stop = open(sys.argv[1], encoding = 'utf-8')
 
