510a4fc2776c6b567f80d351c09e99065111ea0b
Victor Hieber <vg.hieber@gmail.com>
2025-10-07 13:13:00 +0200

    added comments

--- topmost.py
+++ topmost.py
@@ -4,7 +4,8 @@ import urllib.request
 
 def main():
 
-    if sys.argv[2].startswith("http"):
+    if sys.argv[2].startswith("http"): 
+        # if statement for checking if sys.argv[2] is a url or static
         response = urllib.request.urlopen(sys.argv[2])
         lines = response.read().decode("utf8").splitlines()
         inp_file = lines
@@ -16,20 +17,23 @@ def main():
     for line in inp_file:
         striped_inp_lines = line.strip('\n')
         list_inp_file.append(striped_inp_lines)
+        # strips each line and appends each line to a list
 
 
     eng_stop = open(sys.argv[1], encoding = 'utf-8')
+    # takes the second argument in sys
 
     list_stop_file = []
     for line in eng_stop:
         striped_stop_lines = line.strip()
         list_stop_file.append(striped_stop_lines)
+        # strips each line and appends each line to a list
 
     eng_stop.close()
 
-    t = wordfreq.tokenize(list_inp_file)
-    c = wordfreq.countWords(t,list_stop_file)
-    wordfreq.printTopMost(c,int(sys.argv[3]))
+    t = wordfreq.tokenize(list_inp_file) # calls the function tokenize, we store it as t to add it to countWords
+    c = wordfreq.countWords(t,list_stop_file)# calls the function countWords, we store it as c to add it to printTopMost
+    wordfreq.printTopMost(c,int(sys.argv[3]))# calls the function printTopMost
 
 
 
--- wordfreq.py
+++ wordfreq.py
@@ -1,19 +1,24 @@
-def tokenize(lines):
+def tokenize(lines): 
+    # Tokenize is a function that takes in a list from sys.argv
+    # After taking in the list it goes through every line and seperates each symbol, word and number
     words = []
     for line in lines:
         start = 0
         while start < len(line):
             while start < len(line) and line[start].isspace():
+                # If its a space we jump to the next symbol
                 start += 1
             if start >= len(line):
                 break
             if line[start].isdigit():
+                # If its a digit it goes through it
                 end = start
                 while end < len(line) and line[end].isdigit():
                     end += 1
                 words.append(line[start:end])
                 start = end
             elif line[start].isalpha():
+                # If its a letter it goes through it
                 end = start
                 while end < len(line) and line[end].isalpha():
                     end += 1
@@ -25,21 +30,25 @@ def tokenize(lines):
     return words
 
 def countWords(words, stopWords):
+    # countWords is the function that counts how often words occur
     frequencies = {}
     for word in words:
         if word not in stopWords:
             if word not in frequencies:
+                # if the word doesnt exist, we add to a dictionairy
                 frequencies[word] = 1
             else:
+                # if the word already exist, we add 1 to the dictionary
                 frequencies[word] += 1
     return frequencies
 
 def printTopMost(frequencies,n):
+    # printTopMost is the function that prints and sorts each word and freq in a dictioanry
     sort_freq = dict(sorted(frequencies.items(), key = lambda item : item[1], reverse = True))
-    rows = []
+    # we use key = lambda to sort it and reverse = True to reverse the dictionary
     for word, freq in list(sort_freq.items())[:n]:
-        rows.append(print(word.ljust(20) + str(freq).rjust(5)))
-    return rows
+        print(word.ljust(20) + str(freq).rjust(5))
+        # prints the dictionary
     
             
         
